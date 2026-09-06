07cd636356b678c2b955ab10b77d63a145fc8259
Susritha Gudimetla <161280408+SusrithaG1908@users.noreply.github.com>
2026-09-06 21:26:55 +0530

    modified hello c program

--- hello.c
+++ hello.c
@@ -5,5 +5,6 @@ int main()
     printf("Hello from Jenkins CI - Build 2!\n");
     printf("Hello from Jenkins CI - Build 2!\n");
     printf("bye from jenkins!!!");
+    printf("extended job 2 to configure scm to monitor every 2 min");
     return 0;
 }
