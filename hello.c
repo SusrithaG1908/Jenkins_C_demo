aed2a668ee8ccff6d43741e4cb4766b08a41b8cc
Susritha Gudimetla <161280408+SusrithaG1908@users.noreply.github.com>
2026-09-15 09:46:50 +0530

    github actions practice

--- hello.c
+++ hello.c
@@ -6,5 +6,6 @@ int main()
     printf("Hello from Jenkins CI - Build 2!\n");
     printf("bye from jenkins!!!");
     printf("extended job 2 to configure scm to monitor every 2 min");
+    printf("configuring cicd pipeline via github actions");
     return 0;
 }
